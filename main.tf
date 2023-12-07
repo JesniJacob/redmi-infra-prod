4664673e7e5a2a68c083d658e9fdc34699e1a7b4
jesni <jesni@gmail.com>
2023-12-07 07:26:47 +0000

    remote sg created

--- main.tf
+++ main.tf
@@ -25,6 +25,13 @@ resource "aws_security_group" "http_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
