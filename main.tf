c689d79e5ae294de0ab94a851b7b4b760b4ff10a
jesni <jesni@gmail.com>
2023-12-07 07:24:13 +0000

    webaccess sg created

--- main.tf
+++ main.tf
@@ -7,4 +7,35 @@ resource "aws_key_pair" "mykey" {
     owner   = var.project_owner
   }
 }
+resource "aws_security_group" "http_access" {
+  name        = "${var.project_name}-${var.project_env}-http_access"
+  description = "${var.project_name}-${var.project_env}-http_access"
 
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-http_access"
+    Project = var.project_env
+    owner   = var.project_owner
+  }
+}
